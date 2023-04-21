807e5c0dc2443d1d0c9a9aa5772fbf0a58e31e25
Tong <1978572189@qq.com>
2023-04-21 11:52:00 +0800

    Update streamlit_show.py

--- streamlit_show.py
+++ streamlit_show.py
@@ -136,8 +136,10 @@ if st.button("Get Inference"):
         st.image(input_image, use_column_width=True)
         # 图片预处理
         input_image = image_preprocess(input_image)
+        # 转换为PIL格式
+        showimg = Image.open(input_image[0])
         #展示预处理后的图片
-        st.image(input_image[0], use_column_width=True)
+        st.image(showimg, use_column_width=True)
         # 加载模型
         PATH = 'myvit.pth'
         model = ViT_Model(image_size=224, patch_size=16, embed_size=768, num_classes=176,
